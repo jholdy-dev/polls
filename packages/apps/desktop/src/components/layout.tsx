317dd31a481776f7d7fd7f79b7134c30993ed979
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-02-14 15:18:32 -0300

    feat: Add notifications icon to layout

--- packages/apps/desktop/src/components/layout.tsx
+++ packages/apps/desktop/src/components/layout.tsx
@@ -9,9 +9,11 @@ import List from '@mui/material/List'
 import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
+import Badge from '@mui/material/Badge'
 import Container from '@mui/material/Container'
 import MenuIcon from '@mui/icons-material/Menu'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import NotificationsIcon from '@mui/icons-material/Notifications'
 import { MainListItems } from './list-items'
 
 const drawerWidth: number = 240
@@ -104,6 +106,11 @@ export function Layout({ children }: DashboardProps) {
           >
             Dashboard
           </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
