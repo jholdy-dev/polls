1f121fbe1bc7d5103557e0fe6616b4417ee0a686
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-02-14 15:16:50 -0300

    feat(create-quiz): Add ability to create quizzes with multiple questions

--- packages/apps/frontend/src/pages/quizzes/create-quiz.tsx
+++ packages/apps/frontend/src/pages/quizzes/create-quiz.tsx
@@ -3,18 +3,28 @@ import { CreateQuizDto, createQuizDtoSchema } from '@lib/schema'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
-import { useForm } from 'react-hook-form'
+import { useFieldArray, useForm } from 'react-hook-form'
 import { useSnackbarStore, useTabsUserStore } from '../../stores'
 import { quizzesService } from '../../services'
+import { Card, CardContent, CardActions } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+import AddIcon from '@mui/icons-material/Add'
+import { v4 as uuid } from 'uuid'
 
 export function CreateQuiz() {
   const {
     register,
     handleSubmit,
     reset,
+    control,
     formState: { errors },
   } = useForm<CreateQuizDto>({
     resolver: zodResolver(createQuizDtoSchema),
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'questions',
   })
   const { setTab } = useTabsUserStore()
   const { opened } = useSnackbarStore()
@@ -67,10 +77,59 @@ export function CreateQuiz() {
         inputProps={{
           inputMode: 'numeric',
         }}
-        {...register('userId')}
+        {...register('userId', {
+          setValueAs: (value) => Number(value),
+        })}
         error={!!errors.userId}
         helperText={errors.userId?.message}
       />
+      <Card sx={{ p: 2 }}>
+        <h3>Questions</h3>
+        {fields.map((_, index) => (
+          <Card key={uuid()}>
+            <CardContent>
+              <h4>Edit Question</h4>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="description"
+                {...register(`questions.${index}.description` as any)}
+              />
+            </CardContent>
+            <CardActions
+              disableSpacing
+              sx={{
+                alignSelf: 'stretch',
+                display: 'flex',
+                justifyContent: 'flex-end',
+                alignItems: 'flex-start',
+              }}
+            >
+              <Button
+                onClick={() => remove(index)}
+                aria-label="delete"
+                variant="outlined"
+                color="error"
+                size="small"
+              >
+                <DeleteIcon fontSize="small" />
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+        <CardActions>
+          <Button
+            onClick={() => append({ description: '', id: 0 })}
+            aria-label="add"
+            variant="contained"
+            color="success"
+            size="small"
+          >
+            <AddIcon fontSize="small" />
+          </Button>
+        </CardActions>
+      </Card>
       <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
         Save
       </Button>
