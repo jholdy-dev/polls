d58115afe61cc607b8138e3c066ff081cb1ccaf6
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-02-16 11:35:25 -0300

    feat(questions): add create, findAll, findOne, update, and remove methods

--- packages/apps/backend/src/modules/questions/questions.controller.ts
+++ packages/apps/backend/src/modules/questions/questions.controller.ts
@@ -16,6 +16,8 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
 import { ZodPipe } from 'src/core/pipes/zod-pipe'
 import { createQuestionDtoSchema, updateQuestionDtoSchema } from '@lib/schema'
 import { AuthGuard } from '../auth/auth.guard'
+import { GenericResponse } from 'src/dto/generic-response.dto'
+import { Question } from './entities/question.entity'
 
 @ApiBearerAuth()
 @ApiTags('questions')
@@ -25,28 +27,37 @@ export class QuestionsController {
 
   @UseGuards(AuthGuard)
   @Post()
-  create(
+  async create(
     @Param('quizId') quizId: number,
     @Body(new ZodPipe(createQuestionDtoSchema))
     createQuestionDto: CreateQuestionDto,
-  ) {
-    return this.questionsService.create(quizId, createQuestionDto)
+  ): Promise<GenericResponse<Question>> {
+    const result = await this.questionsService.create(quizId, createQuestionDto)
+    return { data: result }
   }
 
   @UseGuards(AuthGuard)
   @Get()
-  async findAll(@Param('quizId') quizId: string) {
-    return await this.questionsService.findAll(quizId)
+  async findAll(
+    @Param('quizId') quizId: string,
+  ): Promise<GenericResponse<Question[]>> {
+    const result = await this.questionsService.findAll(quizId)
+    return { data: result }
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
-  async findOne(@Param('quizId') quizId: string, @Param('id') id: string) {
+  async findOne(
+    @Param('quizId') quizId: string,
+    @Param('id') id: string,
+  ): Promise<GenericResponse<Question>> {
     const question = await this.questionsService.findOne(quizId, +id)
     if (!question) {
       throw new NotFoundException('Question not found')
     }
-    return question
+    return {
+      data: question,
+    }
   }
 
   @UseGuards(AuthGuard)
@@ -56,7 +67,7 @@ export class QuestionsController {
     @Param('id') id: string,
     @Body(new ZodPipe(updateQuestionDtoSchema))
     updateQuestionDto: UpdateQuestionDto,
-  ) {
+  ): Promise<GenericResponse<Question>> {
     const updatedQuestion = await this.questionsService.update(
       quizId,
       +id,
@@ -65,16 +76,23 @@ export class QuestionsController {
     if (!updatedQuestion) {
       throw new NotFoundException('Question not found')
     }
-    return updatedQuestion
+    return {
+      data: updatedQuestion,
+    }
   }
 
   @UseGuards(AuthGuard)
   @Delete(':id')
-  async remove(@Param('quizId') quizId: string, @Param('id') id: string) {
+  async remove(
+    @Param('quizId') quizId: string,
+    @Param('id') id: string,
+  ): Promise<GenericResponse<{ success: boolean }>> {
     const deletedQuestion = await this.questionsService.remove(quizId, +id)
     if (!deletedQuestion) {
       throw new NotFoundException('Question not found')
     }
-    return deletedQuestion
+    return {
+      data: deletedQuestion,
+    }
   }
 }
